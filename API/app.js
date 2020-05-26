c19838455889af96be86e4948543a72b7e5f5237
Filip Jinstrand <filip@jinstrand.se>
2020-05-26 13:41:14 +0200

    Added array filter for cities

--- API/app.js
+++ API/app.js
@@ -3,11 +3,24 @@ const app = express();
 const port = 3000;
 const fs = require('fs');
 
+// Get all countries
 app.get('/country', (req, res) => {
     fs.readFile('land.json', (err, data) => {
         if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.write(data);
+        var countries = JSON.parse(data);
+        res.json(countries);
+        res.end();
+    })
+})
+
+// Get City depending on the country id
+app.get('/country/:id', (req, res) => {
+    var id = parseInt(req.params.id);
+    fs.readFile('stad.json', (err, data) => {
+        if (err) throw err;
+        var cities = JSON.parse(data);
+        var country = cities.filter(a => a.countryid === id);
+        res.json(country);
         res.end();
     })
 })
